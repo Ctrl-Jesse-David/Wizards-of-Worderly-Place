f6df2f971a290e1a9b2ca1d2abe55a4716a19085
Jesse David Francisco <francisco.jessedavid@gmail.com>
2025-03-23 04:30:48 +0800

    new edits in beta-test branch

---
+++ game_logic.py
@@ -0,0 +1,17 @@
+class GameGrid:
+    def __init__(self, grid, positions):
+        pass
+
+
+class WordscapesGame:
+    def __init__(self, letters, words, grids, positions):
+        self.letters = letters
+        self.words = words
+        self.grids = grids
+        self.positions = positions
+    
+    def play(self):
+        pass
+
+game = WordscapesGame(letters, words, grid, positions)
+game.play()
