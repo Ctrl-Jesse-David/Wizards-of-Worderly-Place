81e76f143de3f3dd31142e62857488ded0d2fbc7
Jesse David Francisco <francisco.jessedavid@gmail.com>
2025-03-23 04:50:47 +0800

    small changes ni jesse pinkman

--- game_logic.py
+++ game_logic.py
@@ -1,7 +1,22 @@
+import os
+from game_levels import letters, words, grid, positions
+
 class GameGrid:
     def __init__(self, grid, positions):
-        pass
+        self.grid = grid
+        self.positons = positions
 
+    def refresh_display(self):
+        return os.system('cls' if os.name == 'nt' else 'clear')
+                         
+    def display_grid(self, grid):
+        print('WELCOME TO WORDSCAPES')
+        for row in self.grid:
+            print(''.join(row))
+        print('\n')
+
+    def update_grid(self, word):
+        pass
 
 class WordscapesGame:
     def __init__(self, letters, words, grids, positions):
