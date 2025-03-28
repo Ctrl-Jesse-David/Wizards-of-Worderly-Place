968313d9aa1e797bcb59ac0ac86ab3728e5d24c4
Jesse David Francisco <francisco.jessedavid@gmail.com>
2025-03-28 17:43:45 +0800

    new changes
    updated the point calculator function, removed and updated some conditions/function calls sa self.play(), updated self.the_guess() function para iba yung magpapakita if guess is valid (like yung letters used are in the pool of letters) and guess is not valid

--- game_logic.py
+++ game_logic.py
@@ -1,5 +1,5 @@
 import os, time
-from game_levels import letters, words, grid, positions
+from game_levels import letters, grid, positions
 import random
 
 class GameGrid:
@@ -26,9 +26,9 @@ class GameGrid:
 
 
 class WordscapesGame:
-    def __init__(self, letters, words, grid, positions):
+    def __init__(self, letters, grid, positions):
         self.letters = letters
-        self.words = words
+        self.words = set(positions.keys())
         self.grid = GameGrid(grid, positions)
         self.positions = positions
         self.found_words = set()
@@ -44,14 +44,16 @@ class WordscapesGame:
     def is_valid(self, guess):
         return all(guess.count(letter) <= self.letters.count(letter) 
                    for letter in guess)
+    
+    def calculate_points(self, guess, found_words):
+        return len(set(self.positions[guess]) - \
+            set(coordinate for word in found_words 
+                for coordinate in self.positions[word]))
 
     def play(self):
-        while len(self.found_words) < len(self.words) and self.lives > 0:
+        while self.found_words != self.words and self.lives > 0:
 
             self.grid.display_grid()
-
-            if self.words == self.found_words:
-                break
             self.cur_state()
 
             guess = input('\nGuess a word: ').upper()
@@ -60,12 +62,15 @@ class WordscapesGame:
                 self.shuffle_letters()
                 continue
             
-            elif guess == 'EXIT': #MAY BUGS ATA KAPAG NAGEEXIT
-                time.sleep(0.35)
+            elif guess == 'EXIT': 
+                # MAY BUGS ATA KAPAG NAGEEXIT - N
+                # Anong bug cuh - D
                 break
 
             self.the_guess(guess)
-
+            
+        self.grid.refresh_display()
+        self.grid.display_grid()
         self.end_game()
 
     def cur_state(self):
@@ -77,30 +82,39 @@ class WordscapesGame:
         print("Commands: [shuffle] to shuffle letters, [exit] to quit\n")
 
     def the_guess(self, guess):
-        if guess in self.words and guess not in self.found_words:
-            self.found_words.add(guess)
-            self.grid.update_grid(guess)
-            self.last_guess = guess
-            self.points += 1 #PER NEW LETTER DISC DAPAT UNG POINTS PERO NEXT TIME NA
-            print('Correct!')
+        if self.is_valid(guess):
+            if guess in self.words and guess not in self.found_words:
+                self.grid.update_grid(guess)
+                self.last_guess = guess
+                self.points += self.calculate_points(guess, self.found_words)
+                self.found_words.add(guess)
+                print('Correct!')
+
+            elif guess in self.words and guess in self.found_words:
+                print('Word has already been found.')
+            
+            else:
+                print('Incorrect.')
 
         else:
-            print('Invalid word')
+            print(f"Invalid word! Only {'-'.join(list(self.letters))} is allowed")
             self.lives -= 1
+            time.sleep(1)
+            return
 
         time.sleep(0.35)        
 
     def end_game(self):
+        print(f"WORDS: {', '.join(self.words)}")
+        print(f"FOUND WORDS: {', '.join(self.found_words) if self.found_words else None}") 
+        print(f"SCORE: {self.points}\n")
+
         if len(self.found_words) == len(self.words):
-            print('\nCongratulations! You guessed all the words.\n')
+            print('Congratulations! You guessed all the words.\n')
         else:
             print('Game Over! Thank you for playing!\n')
 
-        print(f"Words: {', '.join(self.words)}")
-        print(f"Found words: {', '.join(self.found_words)}\n")
-        
-        # oks na ong
 
 if __name__ == "__main__":
-    game = WordscapesGame(letters, words, grid, positions)
+    game = WordscapesGame(letters, grid, positions)
     game.play()
